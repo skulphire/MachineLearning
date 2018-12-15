eff3bf668eb8f735b527bbd29eefbb57ffba9dad
ckinney <cdkrobotics@gmail.com>
2018-12-15 03:20:16 -0500

    classifying and using linear regression

--- Regression/test1.py
+++ Regression/test1.py
@@ -7,6 +7,10 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, svm, model_selection
+from sklearn.linear_model import LinearRegression
+
 
 quandl.ApiConfig.api_key = "3DCuBTNWeAPEK6RotBW-"
 df = quandl.get('WIKI/GOOGL')
@@ -32,4 +36,24 @@ forecast_out = int(math.ceil(0.01*len(df))) #predict out 10% of df
 df['label'] = df[forecast_col].shift(-forecast_out) # adjusting feature negativaly to predict 10 days later
 
 df.dropna(inplace=True)
-print(df.head())
+# print(df.head())
+
+#features = X
+#labels = y
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test,y_test)
+
+print(forecast_out)
+print(accuracy)
+
+
