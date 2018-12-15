f44b17cd9c4f81b9d2a847acce4297b18c0bffab
ckinney <cdkrobotics@gmail.com>
2018-12-15 02:42:52 -0500

    quandl request with percents

--- Regression/test1.py
+++ Regression/test1.py
@@ -6,7 +6,14 @@
 
 import pandas as pd
 import quandl
-
+quandl.ApiConfig.api_key = "3DCuBTNWeAPEK6RotBW-"
 df = quandl.get('WIKI/GOOGL')
 
-print(df)
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+
+df['HL_PCT'] = (df['Adj. High']-df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_Change'] = (df['Adj. Close']-df['Adj. Open']) / df['Adj. Open'] * 100
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
+
+print(df.head())
