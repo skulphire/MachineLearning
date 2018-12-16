b700b6ca3a26495bcf95e0de1a0e537e94d96551
ckinney <cdkrobotics@gmail.com>
2018-12-16 01:50:26 -0500

    nn model completed

--- TensorFlow-CPU/tf-handwrittenNN.py
+++ TensorFlow-CPU/tf-handwrittenNN.py
@@ -18,12 +18,25 @@ y = tf.placeholder('float')
 
 def neural_network_model(data):
     # input data * weights + bias
+
     hiddenlayer1 = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
                     'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
-    hiddenlayer3 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+    hiddenlayer2 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
                     'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
     hiddenlayer3= {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
                     'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
     outputlayer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
                     'biases': tf.Variable(tf.random_normal(n_classes))}
 
+    L1 = tf.add(tf.matmul(data,hiddenlayer1['weights']) + hiddenlayer1['biases'])
+    L1 = tf.nn.relu(L1)
+
+    L2 = tf.add(tf.matmul(L1, hiddenlayer2['weights']) + hiddenlayer2['biases'])
+    L2 = tf.nn.relu(L2)
+
+    L3 = tf.add(tf.matmul(L2, hiddenlayer3['weights']) + hiddenlayer3['biases'])
+    L3 = tf.nn.relu(L3)
+
+    Loutput = tf.matmul(L3, outputlayer['weights']) + outputlayer['biases']
+
+    return Loutput
