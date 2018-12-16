7891a3d869ceefb32d8cd65e5a1581fab881bf27
ckinney <cdkrobotics@gmail.com>
2018-12-16 02:06:18 -0500

    training

--- TensorFlow-CPU/tf-handwrittenNN.py
+++ TensorFlow-CPU/tf-handwrittenNN.py
@@ -63,3 +63,5 @@ def train_neural_network(x):
         correct = tf.equal(tf.arg_max(predicition,1),tf.arg_max(y,1))
         accuracy = tf.reduce_mean(tf.cast(correct,'float'))
         print('Accuracy: ',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+train_neural_network(x)
