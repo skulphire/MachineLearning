7f2b9176de663804ec5a1344715149353881feac
ckinney <cdkrobotics@gmail.com>
2018-12-16 17:18:13 -0500

    modified nn from posneg

--- TensorFlowCPU/NNposneg.py
+++ TensorFlowCPU/NNposneg.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 from TensorFlowCPU.posneg import create_featureset_and_lables
+import numpy as np
+
+train_x,train_y,test_x,test_y = create_featureset_and_lables('/home/ckinney/MachineLearning/Datasets/PosNeg_pt5/pos.txt','/home/ckinney/MachineLearning//Datasets/PosNeg_pt5/neg.txt')
+
 
 #10 classes, 0-9
 
@@ -17,7 +21,7 @@ y = tf.placeholder('float')
 def neural_network_model(data):
     # input data * weights + bias
 
-    hiddenlayer1 = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+    hiddenlayer1 = {'weights': tf.Variable(tf.random_normal([len(train_x[0]), n_nodes_hl1])),
                     'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
     hiddenlayer2 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
                     'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
@@ -52,14 +56,21 @@ def train_neural_network(x):
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+
+            i=0
+            while i < len(train_x):
+                start = i
+                end = i+batch_size
+                batch_x = np.array(train_x[start:end])
+                batch_y = np.array(train_y[start:end])
+
+
                 _, c = ses.run([optimizer,cost],feed_dict={x:epoch_x,y:epoch_y})
                 epoch_loss+=c
             print('epoch ',epoch, 'completed out of', hm_epochs, ' loss:',epoch_loss)
 
         correct = tf.equal(tf.arg_max(predicition,1),tf.arg_max(y,1))
         accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('Accuracy: ',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        print('Accuracy: ',accuracy.eval({x:test_x, y:test_y}))
 
 train_neural_network(x)
