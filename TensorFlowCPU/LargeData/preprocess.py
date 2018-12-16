0ff485c042252a3b697ec9bad429148e95e16e1e
ckinney <cdkrobotics@gmail.com>
2018-12-16 18:46:23 -0500

    preprocessing for large data

--- TensorFlowCPU/LargeData/preprocess.py
+++ TensorFlowCPU/LargeData/preprocess.py
@@ -16,28 +16,31 @@ user
 tweet
 '''
 
-def init_process(fin,fout):
-    outfile = open(fout,'a')
+
+def init_process(fin, fout):
+    outfile = open(fout, 'a')
     with open(fin, buffering=200000, encoding='latin-1') as f:
         try:
             for line in f:
-                line = line.replace('"','')
+                line = line.replace('"', '')
                 initial_polarity = line.split(',')[0]
                 if initial_polarity == '0':
-                    initial_polarity = [1,0]
+                    initial_polarity = [1, 0]
                 elif initial_polarity == '4':
-                    initial_polarity = [0,1]
+                    initial_polarity = [0, 1]
 
                 tweet = line.split(',')[-1]
-                outline = str(initial_polarity)+':::'+tweet
+                outline = str(initial_polarity) + ':::' + tweet
                 outfile.write(outline)
         except Exception as e:
             print(str(e))
     outfile.close()
 
+
 #files are on server
 init_process('training.csv','train_set.csv')
 init_process('testing.csv','test_set.csv')
+
 
 def create_lexicon(fin):
     lexicon = []
@@ -47,9 +50,9 @@ def create_lexicon(fin):
             content = ''
             for line in f:
                 counter += 1
-                if (counter/2500.0).is_integer():
+                if (counter / 2500.0).is_integer():
                     tweet = line.split(':::')[1]
-                    content += ' '+tweet
+                    content += ' ' + tweet
                     words = word_tokenize(content)
                     words = [lemmatizer.lemmatize(i) for i in words]
                     lexicon = list(set(lexicon + words))
@@ -58,13 +61,55 @@ def create_lexicon(fin):
         except Exception as e:
             print(str(e))
 
-    with open('lexicon.pickle','wb') as f:
-        pickle.dump(lexicon,f)
+    with open('lexicon-2500-2638.pickle', 'wb') as f:
+        pickle.dump(lexicon, f)
+
 
 create_lexicon('train_set.csv')
 
-def create_test_data_pickle(fin):
 
+def convert_to_vec(fin, fout, lexicon_pickle):
+    with open(lexicon_pickle, 'rb') as f:
+        lexicon = pickle.load(f)
+    outfile = open(fout, 'a')
+    with open(fin, buffering=20000, encoding='latin-1') as f:
+        counter = 0
+        for line in f:
+            counter += 1
+            label = line.split(':::')[0]
+            tweet = line.split(':::')[1]
+            current_words = word_tokenize(tweet.lower())
+            current_words = [lemmatizer.lemmatize(i) for i in current_words]
+
+            features = np.zeros(len(lexicon))
+
+            for word in current_words:
+                if word.lower() in lexicon:
+                    index_value = lexicon.index(word.lower())
+                    # OR DO +=1, test both
+                    features[index_value] += 1
+
+            features = list(features)
+            outline = str(features) + '::' + str(label) + '\n'
+            outfile.write(outline)
+
+        print(counter)
+
+
+convert_to_vec('test_set.csv', 'processed-test-set.csv', 'lexicon-2500-2638.pickle')
+
+
+def shuffle_data(fin):
+    df = pd.read_csv(fin, error_bad_lines=False)
+    df = df.iloc[np.random.permutation(len(df))]
+    print(df.head())
+    df.to_csv('train_set_shuffled.csv', index=False)
+
+
+shuffle_data('train_set.csv')
+
+
+def create_test_data_pickle(fin):
     feature_sets = []
     labels = []
     counter = 0
@@ -83,5 +128,5 @@ def create_test_data_pickle(fin):
     feature_sets = np.array(feature_sets)
     labels = np.array(labels)
 
-create_test_data_pickle('processed-test-set.csv')
 
+create_test_data_pickle('processed-test-set.csv')
