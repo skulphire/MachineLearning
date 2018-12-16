577e234181e55379f40cc65a259a123ef6fa84bb
ckinney <cdkrobotics@gmail.com>
2018-12-16 01:43:25 -0500

    coding mnist nn

---
+++ TensorFlow-CPU/tf-handwrittenNN.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+#10 classes, 0-9
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 100
+
+# 784 values (pixels)
+x = tf.placeholder('float')
+y = tf.placeholder('float')
+
+def neural_network_model(data):
+    # input data * weights + bias
+    hiddenlayer1 = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                    'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
+    hiddenlayer3 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                    'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
+    hiddenlayer3= {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                    'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
+    outputlayer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases': tf.Variable(tf.random_normal(n_classes))}
+
