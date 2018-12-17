243ea8b861bfea22a9fed6f1bbd486e49ff25e8d
ckinney <cdkrobotics@gmail.com>
2018-12-17 01:24:57 -0500

    changed cpu

--- TensorFlowCPU/LargeData/largeDataNN.py
+++ TensorFlowCPU/LargeData/largeDataNN.py
@@ -12,7 +12,7 @@ n_nodes_hl2 = 500
 
 n_classes = 2
 
-batch_size = 400
+batch_size = 32
 total_batches = int(1600000 / batch_size)
 hm_epochs = 10
 
@@ -49,7 +49,7 @@ def train_neural_network(x):
     prediction = neural_network_model(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
-    with tf.Session(config=tf.ConfigProto(inter_op_parallelism_threads=8, intra_op_parallelism_threads=8,)) as sess: #config=tf.ConfigProto(inter_op_parallelism_threads=8, intra_op_parallelism_threads=8,)
+    with tf.Session() as sess: #config=tf.ConfigProto(inter_op_parallelism_threads=8, intra_op_parallelism_threads=8,)
         sess.run(tf.initialize_all_variables())
         try:
             epoch = int(open(tf_log, 'r').read().split('\n')[-2]) + 1
