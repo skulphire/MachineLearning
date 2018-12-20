88f105e6fafe8cf8209deaf6a7b4d2f9461ec559
ckinney <cdkrobotics@gmail.com>
2018-12-20 02:21:21 -0500

    testing conv nn with dist

--- TensorFlowCPU/ConvNetworks/ConvNN-sentdex.py
+++ TensorFlowCPU/ConvNetworks/ConvNN-sentdex.py
@@ -14,13 +14,6 @@ y = tf.placeholder('float')
 keep_rate = 0.8
 keep_prob = tf.placeholder(tf.float32)
 
-#distribute
-cluster = tf.train.ClusterSpec({'worker':['localhost:2222','10.10.1.141:2222']})
-server = tf.train.Server(cluster,task_index=0)
-worker1 = '/job:worker/task:0'
-worker2 = '/job:worker/task:1'
-
-
 def conv2d(x,W):
     return tf.nn.conv2d(x,W,strides=[1,1,1,1], padding='SAME')
 
@@ -28,29 +21,25 @@ def maxpool2d(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding= 'SAME')
 
 def convNN(x):
-    with tf.device(worker1):
-        weights = {'W_conv1':tf.Variable(tf.random_normal([5,5,1,32])),
-                   'W_conv2':tf.Variable(tf.random_normal([5,5,32,64])),
-                   'W_fc':tf.Variable(tf.random_normal([7*7*64,1024])),
-                   'out':tf.Variable(tf.random_normal([1024,n_classes]))}
-    with tf.device(worker2):
-        biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
-                   'b_conv2': tf.Variable(tf.random_normal([64])),
-                   'b_fc': tf.Variable(tf.random_normal([1024])),
-                   'out': tf.Variable(tf.random_normal([n_classes]))}
+    weights = {'W_conv1':tf.Variable(tf.random_normal([5,5,1,32])),
+               'W_conv2':tf.Variable(tf.random_normal([5,5,32,64])),
+               'W_fc':tf.Variable(tf.random_normal([7*7*64,1024])),
+               'out':tf.Variable(tf.random_normal([1024,n_classes]))}
+    biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
+               'b_conv2': tf.Variable(tf.random_normal([64])),
+               'b_fc': tf.Variable(tf.random_normal([1024])),
+               'out': tf.Variable(tf.random_normal([n_classes]))}
 
 
     x = tf.reshape(x,shape=[-1,28,28,1])
 
-    with tf.device(worker1):
-        #1 layer
-        conv1 = tf.nn.relu(conv2d(x,weights['W_conv1']) + biases['b_conv1'])
-        conv1 = maxpool2d(conv1)
+    #1 layer
+    conv1 = tf.nn.relu(conv2d(x,weights['W_conv1']) + biases['b_conv1'])
+    conv1 = maxpool2d(conv1)
 
-    with tf.device(worker2):
-        #2 layer
-        conv2 = tf.nn.relu(conv2d(conv1,weights['W_conv2']) + biases['b_conv2'])
-        conv2 = maxpool2d(conv2)
+    #2 layer
+    conv2 = tf.nn.relu(conv2d(conv1,weights['W_conv2']) + biases['b_conv2'])
+    conv2 = maxpool2d(conv2)
 
     fc = tf.reshape(conv2,shape=[-1,7*7*64])
     fc = tf.nn.relu(tf.matmul(fc,weights['W_fc'])+biases['b_fc'])
@@ -68,7 +57,7 @@ def train_neural_network(x, hm_epochs=10):
 
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    with tf.Session(server.target) as ses:
+    with tf.Session() as ses:
         ses.run(tf.global_variables_initializer())
 
         for epoch in range(hm_epochs):
